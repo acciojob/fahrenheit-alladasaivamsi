a60b9f20c182b40c73ce4b5f0bdf22214b394eee
alladasaivamsi <109912485+alladasaivamsi@users.noreply.github.com>
2023-05-17 20:08:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function toFahrenheit(value) {
+	let fahrenheitValue = value.map((input) => {
+		((input * 9)/5) + 32;
+		return input;
+	});
+};
+
+toFahrenheit();
